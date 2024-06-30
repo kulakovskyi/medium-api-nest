a64726f8058a36f304f5e9d5af2226555e8979b4
Andriy <andriy@B08-OMK-2083146.local>
2024-06-30 18:50:09 +0300

    update articles

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -1,7 +1,11 @@
 import {
   Body,
   Controller,
+  Delete,
+  Get,
+  Param,
   Post,
+  Put,
   UseGuards,
   UsePipes,
   ValidationPipe,
@@ -12,6 +16,7 @@ import { AuthGuard } from '../user/guards/auth.guard';
 import { User } from '../user/decorators/user.decorator';
 import { UserEntity } from '../user/user.entity';
 import { ArticleResponseInterface } from './types/article-response.interface';
+import { DeleteResult } from 'typeorm';
 
 @Controller('article')
 export class ArticleController {
@@ -30,5 +35,36 @@ export class ArticleController {
       createArticleDto,
     );
     return this.articleService.buildArticleResponse(article);
+  }
+
+  @Get(':slug')
+  async getArticle(
+    @Param('slug') slug: string,
+  ): Promise<ArticleResponseInterface> {
+    return this.articleService.getArticle(slug);
+  }
+
+  @Delete(':slug')
+  @UseGuards(AuthGuard)
+  async deleteArticle(
+    @User('id') currentUserId: number,
+    @Param('slug') slug: string,
+  ): Promise<{ success: string }> {
+    return this.articleService.deleteArticle(slug, currentUserId);
+  }
+
+  @Put(':slug')
+  @UseGuards(AuthGuard)
+  @UsePipes(new ValidationPipe())
+  async updateArticle(
+    @User('id') currentUserId: number,
+    @Param('slug') slug: string,
+    @Body('article') updateArticleDto: CreateArticleDto,
+  ): Promise<ArticleResponseInterface> {
+    return this.articleService.updateArticle(
+      slug,
+      currentUserId,
+      updateArticleDto,
+    );
   }
 }
--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -1,10 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateArticleDto } from './dto/create-article.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ArticleEntity } from './article.entity';
-import { Repository } from 'typeorm';
+import { DeleteResult, Repository } from 'typeorm';
 import { UserEntity } from '../user/user.entity';
 import { ArticleResponseInterface } from './types/article-response.interface';
+import slugify from 'slugify';
 
 @Injectable()
 export class ArticleService {
@@ -20,10 +21,62 @@ export class ArticleService {
     const article = new ArticleEntity();
     Object.assign(article, createArticleDto);
     article.author = currentUser;
-    article.slug =
-      encodeURI(createArticleDto.title.toLowerCase().replace(' ', '-')) +
-      (Math.random() * Math.pow(36, 6)).toString(36);
+    article.slug = this.getSlug(createArticleDto.title);
     return await this.articleRepository.save(article);
+  }
+
+  async getArticle(slug: string): Promise<ArticleResponseInterface> {
+    const article = await this.articleRepository.findOne({ slug });
+    if (!article) {
+      throw new HttpException('Article not found', HttpStatus.NOT_FOUND);
+    }
+    return this.buildArticleResponse(article);
+  }
+
+  async deleteArticle(
+    slug: string,
+    currentUserId: number,
+  ): Promise<{ success: string }> {
+    const article = await this.articleRepository.findOne({ slug });
+    if (!article) {
+      throw new HttpException('Article not found', HttpStatus.NOT_FOUND);
+    }
+    if (article.author.id !== currentUserId) {
+      throw new HttpException(
+        'You are not an author of this article',
+        HttpStatus.FORBIDDEN,
+      );
+    }
+    await this.articleRepository.remove(article);
+    return { success: 'Article delete' };
+  }
+
+  async updateArticle(
+    slug: string,
+    currentUserId: number,
+    updateArticleDto: CreateArticleDto,
+  ): Promise<ArticleResponseInterface> {
+    const article = await this.articleRepository.findOne({ slug });
+    if (!article) {
+      throw new HttpException('Article not found', HttpStatus.NOT_FOUND);
+    }
+    if (article.author.id !== currentUserId) {
+      throw new HttpException(
+        'You are not an author of this article',
+        HttpStatus.FORBIDDEN,
+      );
+    }
+    Object.assign(article, updateArticleDto);
+    await this.articleRepository.save(article);
+    return this.buildArticleResponse(article);
+  }
+
+  private getSlug(title: string): string {
+    return (
+      slugify(title, { lower: true }) +
+      '-' +
+      (Math.random() * Math.pow(36, 6)).toString(36)
+    );
   }
 
   buildArticleResponse(article: ArticleEntity): ArticleResponseInterface {
