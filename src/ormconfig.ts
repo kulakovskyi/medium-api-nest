2247c7ce41a95f3c46b520b25ad1e9b7cc23cd3a
kulakovskyi <torchok9@gmail.com>
2024-07-01 17:49:48 +0300

    update

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -2,17 +2,20 @@ import { ConnectionOptions } from 'typeorm';
 
 const config: ConnectionOptions = {
   type: 'postgres',
-  host: 'localhost',
+  host: 'dpg-cq1bu4mehbks73f8ie9g-a.frankfurt-postgres.render.com',
   port: 5432,
-  username: 'mediumclone',
-  password: '123',
-  database: 'mediumclone',
+  username: 'medium_db_user',
+  password: 'WBWaBP3BAStzcDfGTnlPeogOkenSEAwX',
+  database: 'medium_db',
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
   synchronize: false,
   migrationsRun: true,
   migrations: [__dirname + '/migrations/*{.ts,.js}'],
   cli: {
     migrationsDir: 'src/migrations',
+  },
+  ssl: {
+    rejectUnauthorized: false,
   },
 };
 
