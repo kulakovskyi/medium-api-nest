af3acf7b411f65fe80a3ed479a1cc428a39bb7c7
kulakovskyi <torchok9@gmail.com>
2024-07-01 17:24:46 +0300

    add auth

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,10 @@ async function bootstrap() {
     .setTitle('Medium api')
     .setDescription('Docs for the medium clone api')
     .setVersion('1.0')
+    .addBearerAuth(
+      { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
+      'access-token',
+    )
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('doc', app, document);
